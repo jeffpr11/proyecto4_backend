7771adad54255c5d5478ff1d78715a9a3f098bf0
Jeffpr11 <Jefgapra@espol.edu.ec>
2022-12-02 01:53:29 -0500

    user app

---
+++ user/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+from utils.models import BaseModel
+
+
+class Profile(BaseModel):
+    class Genre(models.TextChoices):
+        FEMENINO = 'F'
+        MASCULINO = 'M'
+    
+    class CivilStatus(models.TextChoices):
+        CASADO = 'C'
+        SOLTERO = 'S'
+        
+    class Role(models.IntegerChoices):
+        ADMINISTRADOR = 0
+        LIDER = 1
+        MIEMBRO = 2
+
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING, related_name='profile')
+    card_id = models.CharField(max_length=10, editable=False)
+    born_date = models.DateField(auto_now=False)
+    genre = models.CharField(choices=Genre.choices, default=Genre.FEMENINO, max_length=1)
+    civil_status = models.CharField(choices=CivilStatus.choices, default=CivilStatus.SOLTERO, max_length=1)
+    tel_1 = models.CharField(max_length=10)
+    tel_2 = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    work = models.CharField(max_length=50)
+    work_address = models.CharField(max_length=50)
+    work_activity = models.CharField(max_length=50)
+    work_tel = models.CharField(max_length=10)
+    card_id_resource = models.OneToOneField('organization.Resource', on_delete=models.DO_NOTHING)
+    role = models.IntegerField(choices=Role.choices, default=Role.MIEMBRO)
+
+    def __str__(self):
+        return self.name
