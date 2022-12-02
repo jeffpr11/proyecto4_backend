1268065fae4e69caf7ced5e47baf2c5341291e21
Jeffpr11 <Jefgapra@espol.edu.ec>
2022-12-02 01:52:59 -0500

    organization app

---
+++ organization/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from utils.models import BaseModel
+
+
+class Group(BaseModel):
+    principal_group = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    level = models.SmallIntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Resource(BaseModel):
+    name = models.CharField(max_length=50)
+    route = models.CharField(max_length=100)
+    groups = models.ManyToManyField('Group')
+    
+    def __str__(self):
+        return self.name
