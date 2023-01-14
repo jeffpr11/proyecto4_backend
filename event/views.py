78cfff969904553d272230f0e82f021d8cb67442
Jeffpr11 <Jefgapra@espol.edu.ec>
2023-01-14 14:11:02 -0500

    event/record/comment api update

--- event/views.py
+++ event/views.py
@@ -2,23 +2,163 @@ from rest_framework import viewsets
 from .models import *
 from .serializers import *
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
     serializer_class = EventSerializer
     filter_backends = [DjangoFilterBackend]
 
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Event.objects.all().order_by('-date_created')
+        else:
+            return Event.objects.filter(state=0).order_by('-date_created')
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_creator'] = username
+        data['user_modifier'] = username
+        data['state'] = 0
+        data['last_action'] = 0
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_modifier'] = username
+        data['last_action'] = 1
+
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        if not self.request.user.is_staff:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        instance = self.get_object()
+
+        username = request.user.get_full_name()[:30]
+        instance.user_modifier = username
+        instance.state = 1
+        instance.last_action = 2
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class RecordViewSet(viewsets.ModelViewSet):
-    queryset = Record.objects.all()
     serializer_class = RecordSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['event']
 
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Record.objects.all().order_by('-date_created')
+        else:
+            return Record.objects.filter(state=0).order_by('-date_created')
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_creator'] = username
+        data['user_modifier'] = username
+        data['state'] = 0
+        data['last_action'] = 0
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_modifier'] = username
+        data['last_action'] = 1
+
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        if not self.request.user.is_staff:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        instance = self.get_object()
+
+        username = request.user.get_full_name()[:30]
+        instance.user_modifier = username
+        instance.state = 1
+        instance.last_action = 2
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['event']
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Comment.objects.all().order_by('-date_created')
+        else:
+            return Comment.objects.filter(state=0).order_by('-date_created')
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_creator'] = username
+        data['user_modifier'] = username
+        data['state'] = 0
+        data['last_action'] = 0
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()
+
+        username = request.user.get_full_name()[:30]
+        data['user_modifier'] = username
+        data['last_action'] = 1
+
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        if not self.request.user.is_staff:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        instance = self.get_object()
+
+        username = request.user.get_full_name()[:30]
+        instance.user_modifier = username
+        instance.state = 1
+        instance.last_action = 2
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
